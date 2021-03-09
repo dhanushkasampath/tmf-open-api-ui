6d8a9f0a7eee3fdcf40f8959a624920a9605ae4d
Dhanushka_105105 <dhanushka.hewakaluannakkage@axiatadigitallabs.com>
2021-03-09 22:10:53 +0530

    added form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QueryCustomerComponent } from './customer-mgt/query-customer/query-customer.component';
+import { CreateCustomerComponent } from './customer-mgt/create-customer/create-customer.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: QueryCustomerComponent },
+  { path: 'add-new-customer', component: CreateCustomerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerMgtComponent } from './customer-mgt/customer-mgt.component';
+import { CreateCustomerComponent } from './customer-mgt/create-customer/create-customer.component';
+import { QueryCustomerComponent } from './customer-mgt/query-customer/query-customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerMgtComponent
+    CustomerMgtComponent,
+    CreateCustomerComponent,
+    QueryCustomerComponent
   ],
   imports: [
     BrowserModule,
