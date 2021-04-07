f18c6fc1ab4ded1706be81a07f821186adc69106
Sadunika <sadunika.kularathna@axiatadigitallabs.com>
2021-04-07 17:05:58 +0530

    form added

--- src/app/customer-mgt/create-customer/create-customer.component.ts
+++ src/app/customer-mgt/create-customer/create-customer.component.ts
@@ -16,60 +16,111 @@ export class CreateCustomerComponent implements OnInit {
 
   ngOnInit(): void {
     this.queryForm = this.formBuilder.group({
-      id: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      name: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      href: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      status: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      type: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      statusReason: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      baseType: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      schemaLocation: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      startDateTime: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      endDateTime: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      accountId: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      accountReferredType: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      accountBaseType: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      accountType: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      accountName: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      accountDescription: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      accountHref: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      accountSchemaLocation: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
+      id: new FormControl(''),
+      name: new FormControl(''),
+      href: new FormControl(''),
+      status: new FormControl(''),
+      type: new FormControl(''),
+      statusReason: new FormControl(''),
+      baseType: new FormControl(''),
+      schemaLocation: new FormControl(''),
+      startDateTime: new FormControl(''),
+      endDateTime: new FormControl(''),
+
+      // account
+      accountId: new FormControl(''),
+      accountReferredType: new FormControl(''),
+      accountBaseType: new FormControl(''),
+      accountType: new FormControl(''),
+      accountName: new FormControl(''),
+      accountDescription: new FormControl(''),
+      accountHref: new FormControl(''),
+      accountSchemaLocation: new FormControl(''),
+
+      // payment
+      paymentId: new FormControl(''),
+      paymentReferredType: new FormControl(''),
+      paymentBaseType: new FormControl(''),
+      paymentType: new FormControl(''),
+      paymentName: new FormControl(''),
+      paymentHref: new FormControl(''),
+      paymentSchemaLocation: new FormControl(''),
+
+      // characteristic
+      characteristicId: new FormControl(''),
+      characteristicName: new FormControl(''),
+      characteristicType: new FormControl(''),
+      characteristicBaseType: new FormControl(''),
+      characteristicValue: new FormControl(''),
+      characteristicValueType: new FormControl(''),
+      characteristicSchemaLocation: new FormControl(''),
+
+      // relatedParty
+      relatedPartyId: new FormControl(''),
+      relatedPartyName: new FormControl(''),
+      relatedPartyHref: new FormControl(''),
+      relatedPartyReferredType: new FormControl(''),
+      relatedPartyType: new FormControl(''),
+      relatedPartyBaseType: new FormControl(''),
+      relatedpartyRole: new FormControl(''),
+      relatedPartySchemaLocation: new FormControl(''),
+
+      // agreement
+      agreementId: new FormControl(''),
+      agreementName: new FormControl(''),
+      agreementHref: new FormControl(''),
+      agreementReferredType: new FormControl(''),
+      agreementType: new FormControl(''),
+      agreementBaseType: new FormControl(''),
+      agreementSchemaLocation: new FormControl(''),
+
+      // engagedPartyRole
+      engagedPartyId: new FormControl(''),
+      engagedPartyName: new FormControl(''),
+      engagedPartyHref: new FormControl(''),
+      engagedPartyReferredType: new FormControl(''),
+      engagedPartyType: new FormControl(''),
+      engagedPartyBaseType: new FormControl(''),
+      engagedPartyRole: new FormControl(''),
+      engagedPartySchemaLocation: new FormControl(''),
+
+      // creditProfile
+      creditProfileId: new FormControl(''),
+      creditProfileDate: new FormControl(''),
+      creditProfilecreditScore: new FormControl(''),
+      creditProfilecreditRiskRating: new FormControl(''),
+      creditProfileType: new FormControl(''),
+      creditProfileBaseType: new FormControl(''),
+      creditProfileSchemaLocation: new FormControl(''),
+      creditProfileStartDateTime: new FormControl(''),
+      creditProfileEndDateTime: new FormControl(''),
+
+      // contactMedium
+      contactMediumId: new FormControl(''),
+      contactMediumMediumType: new FormControl(''),
+      contactMediumPreferred: new FormControl(''),
+      contactMediumBaseType: new FormControl(''),
+      contactMediumReferredType: new FormControl(''),
+      contactMediumSchemaLocation: new FormControl(''),
+      contactMediumStartDateTime: new FormControl(''),
+      contactMediumEndDateTime: new FormControl(''),
+
+      // contact medium characteristics
+      contactMediumCharacteristicId: new FormControl(''),
+      contactMediumCharacteristicCountry: new FormControl(''),
+      contactMediumCharacteristiCity: new FormControl(''),
+      contactMediumCharacteristicContactType: new FormControl(''),
+      contactMediumCharacteristicSocialNetworkId: new FormControl(''),
+      contactMediumCharacteristicEmailAddress: new FormControl(''),
+      contactMediumCharacteristicPhoneNumber: new FormControl(''),
+      contactMediumCharacteristicStateOrProvince: new FormControl(''),
+      contactMediumCharacteristicFaxNumber: new FormControl(''),
+      contactMediumCharacteristicPostCode: new FormControl(''),
+      contactMediumCharacteristicStreet1: new FormControl(''),
+      contactMediumCharacteristicStreet2: new FormControl(''),
+      contactMediumCharacteristicSchemaLocation: new FormControl(''),
+      contactMediumCharacteristicType: new FormControl(''),
+      contactMediumCharacteristicBaseType: new FormControl(''),
     });
   }
 
