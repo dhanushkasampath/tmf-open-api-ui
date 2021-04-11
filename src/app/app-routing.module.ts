4c3632a9e1386b5910edf44c4914a906ccf4242f
Dhanushka_105105 <dhanushka.hewakaluannakkage@axiatadigitallabs.com>
2021-04-11 09:42:52 +0530

    working ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomersViewComponent } from './customer-mgt/customers-view/customers-view.component';
+import { CreateCustomerComponent } from './customer-mgt/create-customer/create-customer.component';
+import { ManageCustomerComponent } from './customer-mgt/manage-customer/manage-customer.component';
+import { QueryCustomerComponent } from './customer-mgt/query-customer/query-customer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CustomersViewComponent },
+  { path: 'query-customer', component: QueryCustomerComponent },
+  { path: 'add-new-customer', component: CreateCustomerComponent },
+  { path: 'view-customer/:id', component: ManageCustomerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,92 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersViewComponent } from './customer-mgt/customers-view/customers-view.component';
+import { CreateCustomerComponent } from './customer-mgt/create-customer/create-customer.component';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CommonModule} from "@angular/common";
+
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatRippleModule} from '@angular/material/core';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatSelectModule} from '@angular/material/select';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import { QueryCustomerComponent } from './customer-mgt/query-customer/query-customer.component';
+import { ManageCustomerComponent } from './customer-mgt/manage-customer/manage-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersViewComponent,
+    CreateCustomerComponent,
+    QueryCustomerComponent,
+    ManageCustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatRippleModule,
+    MatListModule,
+    MatGridListModule,
+    MatStepperModule,
+    MatSelectModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatSlideToggleModule,
+    MatRadioModule,
+    MatTabsModule,
+    MatGridListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
